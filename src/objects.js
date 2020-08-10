ff63a6f19ea0e889258c2d6eb4edc919f9306bf7
Alan Brooks <abrooks@live.co.uk>
2020-08-11 00:32:52 +0100

    7/10 object coding complete

--- src/objects.js
+++ src/objects.js
@@ -1,30 +1,38 @@
 const createPerson = (name, age) => {
-  // your code here
+  return {
+    name: name, 
+    age: age,
+  }
 };
 
 const getName = object => {
-  // your code here
+  return object.name;
 };
 
 const getProperty = (property, object) => {
-  // your code here
+  return object[property];
 };
 
 const hasProperty = (property, object) => {
-  // your code here
+  if (object.hasOwnProperty(property)) {
+    return true; }
+  else {
+    return false; }
 };
 
 const isOver65 = person => {
-  // your code here
+  if (person.age > 65) {
+  return true; } 
+  else {
+  return false; }
 };
 
 const getAges = people => {
-  // your code here
+  const allAges = people.map(person => person.age);
+  return allAges;
 };
 
-const findByName = (name, people) => {
-  // your code here
-};
+const findByName = (name, people) => people.find(obj => obj.name === name);
 
 const findHondas = cars => {
   // your code here
